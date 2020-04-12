88fee397169b12443b04122d406b4a8ce0450422
dsying <dingduoduo0916@gmail.com>
2020-04-12 23:16:04 +0800

    个人博客系统 后端系统搭建 springboot + mybatis + mysql，日志的配置

---
+++ src/main/java/com/dsying/blogbackend/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.dsying.blogbackend.controller;
+
+import com.dsying.blogbackend.entity.User;
+import com.dsying.blogbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+  @Autowired
+  UserService userService;
+
+  @RequestMapping(value = "/all", method = RequestMethod.GET)
+  @ResponseBody
+  public List<User> getAll() {
+    return userService.getAll();
+  }
+}
---
+++ src/main/java/com/dsying/blogbackend/service/UserService.java
@@ -0,0 +1,18 @@
+package com.dsying.blogbackend.service;
+
+import com.dsying.blogbackend.dao.UserMapper;
+import com.dsying.blogbackend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+  @Autowired
+  UserMapper userMapper;
+
+  public List<User> getAll() {
+    return userMapper.getAll();
+  }
+}
